bcf2888c8e9385272812dcd15182cb16dce44659
Daniel Pashmfouroush <139705515+daniel7970@users.noreply.github.com>
2025-05-12 01:23:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+import { createClient } from
+    'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabase = createClient('https://qhosqycogofmmkchauel.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFob3NxeWNvZ29mbW1rY2hhdWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MTk2NjcsImV4cCI6MjA2MjQ5NTY2N30.wa9iQbeFumEJVyT8Bi4Ns4-2ABeYE6g2iG1a70iTAbM');
+
+const buttons = document.querySelector("button");
+const form = document.querySelector("form");
+const name = document.getElementById("name");
+const license = document.getElementById("license");
+const message = document.getElementById("message");
+
+
+buttons.addEventListener("click", () => {
+    checkInputs();
+});
+
+function checkInputs() {
+    
+}
