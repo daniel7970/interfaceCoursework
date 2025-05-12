0de78e193cd417026ce77405b7c082a2fe35f145
Daniel Pashmfouroush <139705515+daniel7970@users.noreply.github.com>
2025-05-12 01:52:05 +0100

    Delete supabaseScript.js

--- supabaseScript.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from
-    'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabase = createClient('https://qhosqycogofmmkchauel.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFob3NxeWNvZ29mbW1rY2hhdWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MTk2NjcsImV4cCI6MjA2MjQ5NTY2N30.wa9iQbeFumEJVyT8Bi4Ns4-2ABeYE6g2iG1a70iTAbM');
